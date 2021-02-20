15095348ddc73d8a9a0390ba37b6d24fc0d16857
rrdhoi <r.ridoi702@gmail.com>
2021-02-20 19:45:19 +0800

    Initial commit

--- app/src/main/java/com/rrdhoi/projectone/SplashScreenActivity.kt
+++ app/src/main/java/com/rrdhoi/projectone/SplashScreenActivity.kt
@@ -7,3 +7,16 @@ import com.rrdhoi.projectone.onboarding.OnBoardingOneActivity
 import java.util.*
 import kotlin.concurrent.schedule
 
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val timer = Timer()
+        timer.schedule(5000) {
+            val intent = Intent(this@SplashScreenActivity, OnBoardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
